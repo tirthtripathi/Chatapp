fcd9209f43aebe59f846f80b376bab918eaf7a70
Tirth Tripathi <tirthtripathi13@gmail.com>
2024-05-30 18:01:34 +0530

    Add vite and prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,20 +22,10 @@ app.use('/api/user', userRoutes);
 app.use('/api/chat',chatRoutes);
 app.use('/api/message', messegeRoutes);
 
-// ---------  Deployment code ---------
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname1,'./frontend/dist')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolv(__dirname1,"frontend","dist","index.html"));
-    });
-} else {
-    app.get('/', (req, res)=>{
+app.get('/', (req, res)=>{
         res.send("Hello user")
-    })
-}
-//  ---------  Deployment code ---------
+})
 
 app.use(notFound)
 app.use(errorHandler)
